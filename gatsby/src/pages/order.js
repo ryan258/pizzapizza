29bea2a92f4665b3fa8842b5ed6b503b9dab7ab5
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-25 13:30:52 -0500

    Create Nav component.

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function OrderPage() {
   return (
     <div>
+      <Nav />
       <p>I'm the order page!</p>
     </div>
   );
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function PizzasPage() {
   return (
     <div>
+      <Nav />
       <p>I'm the pizzas page!</p>
     </div>
   );
